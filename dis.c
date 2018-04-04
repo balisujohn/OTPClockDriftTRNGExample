cc1a439c96a9ad4a0435714de4dcc65285ee234d
John Balis <balis@royal-13.cs.wisc.edu>
2018-04-04 12:42:08 -0500

    added some tests to distribution tester

--- dis.c
+++ dis.c
@@ -3,15 +3,48 @@
 #include<time.h>
 #include<assert.h>
 #include<cyrand.h>
-
+#include<math.h>
 
 #define n 100000
 
 
+int vals[256];
+
+
+
+double average()
+{
+	int sum = 0;
+	for(int i = 0; i < 256; i++)
+	{
+		sum += vals[i]; 
+	}
+	return ((double)sum)/256.0;
+}
+
+
+double stDEV()
+{
+	double std = 0.0;
+	double avg = average();
+	for(int i = 0; i < 256; i++)
+	{
+		std +=  ((double)vals[i]-avg) *  ((double)vals[i]-avg);
+	}
+	return sqrt(std/256.0);
+}
+
+
+double variance()
+{
+return stDEV() * stDEV();
+}
+
+
+
+
 int main (char * argv[], int argc)
 {
-
-	int vals[256];
 
 
 	for(int i = 0; i < 256; i++)
@@ -28,9 +61,13 @@ int main (char * argv[], int argc)
 
 	for(int i = 0; i < 256; i++)
 	{
-		printf("OCCURENCES: %d CHARACTER:%c\n",vals[i],(char)i);
+		printf("CHARACTER:%c OCCURENCES: %d\n",(char)i,vals[i]);
 
 	}
+
+	printf("AVERAGE: %f\n", average());
+	printf("STANDARD DEVIATION: %f\n", stDEV());
+	printf("VARIANCE: %f\n", variance());
 
 
 	return 0;
--- makefile
+++ makefile
@@ -2,7 +2,7 @@
 all : distribution production
 
 distribution: dis.c cyrand.c
-	gcc -o distribution  dis.c cyrand.c  -std=c11 -I.
+	gcc -o distribution  dis.c cyrand.c  -std=c11 -I. -lm
 production: implementation.c cyrand.c
-	gcc -o production  implementation.c cyrand.c  -std=c11 -I.
+	gcc -o production  implementation.c cyrand.c  -std=c11 -I. -lm
 
