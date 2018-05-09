7cc386c866d5b100e1828fcb6c8d23c01c414e35
John Balis <balis@wisc.edu>
2018-05-09 16:19:12 -0500

    added buffered write

--- source.c
+++ source.c
@@ -2,17 +2,27 @@
 #include<stdlib.h>
 #include<cyrand.h>
 #include<time.h>
+#include<unistd.h>
+
+#define bufferLength 100000
 int main (int arcgc, char * argv)
 {
 
-srand(time(0));
+	srand(time(0));
 
-for (;;)
-{
-char c = randByte();
-printf("%c", c);
-}
+	char * writeBuffer = malloc(bufferLength);
+	int writeIndex = 0;
+	for (;;)
+	{
+		writeBuffer[writeIndex] = randByte();
+		writeIndex++; 
+		if( writeIndex == bufferLength)
+		{
+			write(1,writeBuffer,bufferLength);
+			writeIndex = 0;
+		}
+	}
 
 
-return 0;
+	return 0;
 }
